4c5d350b0c2d3454704227788179db40b19944b1
Alberto Pereira <106534698+Betopereiradev@users.noreply.github.com>
2023-03-22 14:25:35 -0300

    v1.2
    Adicionado modal para registro de transações

--- main.js
+++ main.js
@@ -51,3 +51,90 @@ const config = {
 
 // Renderizar o gráfico
 const myChart = new Chart(document.getElementById("meu-grafico"), config);
+
+// Modal de registro de transação
+const botao = document.getElementById("abrirModalTransacao");
+const modal = document.getElementById("modalRegistroTransacao");
+const fechar = document.getElementById("fecharModalTransacao");
+
+botao.onclick = function () {
+  modal.style.display = "block";
+};
+
+fechar.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Valores atuais no dashboard
+// Começa zerado
+
+// Função para registrar a transação
+const botaoRegistrar = document.getElementById("registrarTransacao");
+const saida = document.getElementById("saidas-balanco");
+const entrada = document.getElementById("entradas-balanco");
+const emConta = document.getElementById("conta-balanco");
+
+var valorAtualEntrada = 0;
+// Obter valor armazenado no localStorage
+valorAtualEntrada = parseFloat(localStorage.getItem("valorEntrada")) || 0;
+
+var valorAtualSaida = 0;
+// Obter valor armazenado no localStorage
+valorAtualSaida = parseFloat(localStorage.getItem("valorSaida")) || 0;
+
+var valorAtualConta = 0;
+
+// Exibe os valores armazenados quando a página é carregada
+document.addEventListener("DOMContentLoaded", function () {
+  let valorSaidaArmazenado = localStorage.getItem("valorSaida");
+  saida.textContent = "R$ " + valorSaidaArmazenado;
+
+  let valorEntradaArmazenado = localStorage.getItem("valorEntrada");
+  entrada.textContent = "R$ " + valorEntradaArmazenado;
+
+  let emContaArmazenado = localStorage.getItem("emConta");
+  emConta.textContent = "R$ " + emContaArmazenado;
+});
+
+botaoRegistrar.addEventListener("click", function () {
+  var tipoTransacao = document.getElementById("tipoTransacao").value;
+  if (tipoTransacao == "entrada") {
+    var valorTransacao = document.getElementById("valorTransacao").value;
+    valorTransacao = parseFloat(valorTransacao);
+
+    // Atualizar o valor atual
+    var valorAtualizado = valorAtualEntrada + valorTransacao;
+    valorAtualEntrada = valorAtualizado;
+
+    localStorage.setItem("valorEntrada", valorAtualEntrada);
+
+    // Atualizar o valor exibido na página
+    entrada.textContent = "R$ " + valorAtualEntrada.toFixed(2);
+  } else if (tipoTransacao == "saida") {
+    var valorTransacao = document.getElementById("valorTransacao").value;
+    valorTransacao = parseFloat(valorTransacao);
+
+    // Atualizar o valor atual
+    var valorAtualizado = valorAtualSaida + valorTransacao;
+    valorAtualSaida = valorAtualizado;
+
+    localStorage.setItem("valorSaida", valorAtualSaida);
+
+    // Atualizar o valor exibido na página
+    saida.textContent = "R$ " + valorAtualSaida.toFixed(2);
+  }
+
+  // Atualizar o saldo em conta
+  var saldoEmConta = valorAtualEntrada - valorAtualSaida;
+  emConta.textContent = "R$ " + saldoEmConta.toFixed(2);
+
+  localStorage.setItem("emConta", saldoEmConta);
+
+  // Avisa que foi cadastrada
+  const aviso = document.getElementById("aviso-popup");
+  aviso.textContent = "Transação cadastrada com sucesso.";
+
+  setTimeout(function () {
+    aviso.remove();
+  }, 3000);
+});
